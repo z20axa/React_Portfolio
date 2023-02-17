0e35ca1531d38d2da9f1b796a0dda910bbb65638
z20axa <alejandrospan@hotmail.com>
2023-02-16 23:24:38 -0500

    Relocates the images folder within the src folder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
                     <figure>
                         <header>Project 1 - Nail Salon Appoinment App</header>
                         <a href="https://caltran117.github.io/Nail-Salon-App/" target="_blank"><img class="enlarge-pic"/>
-                                src="../public/images/Web capture_23-12-2022_32539_caltran117.github.io.jpeg"
+                                src="../images/Web_capture_23-12-2022_32539_caltran117.github.io.jpeg"
                                 alt="Nail Salon App"></a>
                     </figure>
                 </div>
@@ -20,7 +20,7 @@ function Projects() {
                     <figure>
                         <header>Project 2 - Gym Member App</header>
                         <a href="https://hemlock-fit-club.herokuapp.com/" target="_blank"><img class="enlarge-pic"/>
-                                src="../public/images/Web capture_23-12-2022_35149_hemlock-fit-club.herokuapp.com.jpeg"
+                                src="../images/Web_capture_23-12-2022_35149_hemlock-fit-club.herokuapp.com.jpeg"
                                 alt="Gym Member App"></a>
                     </figure>
                 </div>
@@ -30,5 +30,4 @@ function Projects() {
   }
 
 export default Projects;
-
 
