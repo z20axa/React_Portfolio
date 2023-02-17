11806c339117e28deec579e904f8a1bde3fe9794
z20axa <alejandrospan@hotmail.com>
2023-02-16 22:19:43 -0500

    Adds revisions to header js component and header css files

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <Navbar />
       <Header />
+      <Navbar />
+
       <Section />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import '../styles/Header.css';
-// import imgUrl from './background.png';
-
-// By importing the Header.css file, it is added to the DOM whenever this component loads
-
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
-
-const styles = {
-  headerStyle: {
-    background: 'transparent',
-    // backgroundImage: 'url(' + imgUrl + ')',
-  },
-  headingStyle: {
-    fontSize: '100px',
-  },
-};
-
-// We use JSX curly braces to evaluate the style object
 
 function Header() {
   return (
-    <header style={styles.headerStyle} className="header">
-      <h1 style={styles.headingStyle}>Welcome</h1>
+    <header>
+        <h1>Alejandro's Professional Porfolio</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#about-me">ABOUT ME</a>
+                </li>
+                <li>
+                    <a href="#projects">PROJECTS</a>
+                </li>
+                <li>
+                    <a href="#contact-me">CONTACT ME</a>
+                </li>
+            </ul>
+        </nav>
     </header>
   );
 }
